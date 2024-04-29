986a160e672cb4da43e0c4c5a8d3959790524ece
Anashm24 <anashoumaid19@gmail.com>
2024-04-30 00:33:09 +0100

    creating the User class

--- models/user.py
+++ models/user.py
@@ -6,10 +6,17 @@ from sqlalchemy import Column
 from sqlalchemy import String
 
 class User(BaseModel, Base):
-    """represent a class User"""
+    """Represents a user for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store users.
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+    """
     __tablename__ = "users"
-    
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    lastname = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
