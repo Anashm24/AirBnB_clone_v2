3fd89d4ff3391a6d946b71e5dffe5f109007c356
Anashm24 <anashoumaid19@gmail.com>
2024-04-29 17:22:40 +0100

    create engine and session

--- models/city.py
+++ models/city.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, ForeignKey
-
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
