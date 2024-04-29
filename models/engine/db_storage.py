9789c8e88a5f32c1bd1c595d2469fdcd51399e6d
Anashm24 <anashoumaid19@gmail.com>
2024-04-29 17:33:59 +0100

    create engine and session

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,7 +17,7 @@ class DBStorage:
     """represent a new engine"""
     __engine = None
     __session = None
-    
+
     def __init__(self):
         db_config = {
             'user': getenv('HBNB_MYSQL_USER'),
@@ -32,7 +32,7 @@ class DBStorage:
         )
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
-    
+
     def all(self, cls=None):
         """Query on the curret database session all objects of the given class.
         If cls is None, queries all types of objects.
@@ -47,21 +47,21 @@ class DBStorage:
             objects.extend(self.__session.query(Review).all())
             objects.extend(self.__session.query(Amenity).all())
         else:
-            if type(cls) == str:
+            if type(cls) is str:
                 cls = eval(cls)
             objects = self.__session.query(cls).all()
         return {f"{type(obj).__name__}.{obj.id}": obj for obj in objects}
 
     def new(self, obj):
-        """add the object to the current 
+        """add the object to the current
         database session"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current 
+        """commit all changes of the current
         database session"""
         self.__session.commit()
-    
+
     def delete(self, obj=None):
         """Delete an object from the session if provided."""
         if obj is not None:
@@ -70,7 +70,7 @@ class DBStorage:
     def reload(self):
         """Reload the database engine and reset the session."""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__engine = Session()
-
