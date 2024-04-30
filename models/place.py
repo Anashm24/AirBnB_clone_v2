bf91cb3f7e61e6ffbedecacecb0ae97b8f072299
Anashm24 <anashoumaid19@gmail.com>
2024-04-30 13:39:23 +0100

    review

--- models/place.py
+++ models/place.py
@@ -6,23 +6,13 @@ from models.base_model import Base
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.review import Review
-from sqlalchemy import Column
-from sqlalchemy import Float
-from sqlalchemy import ForeignKey
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy import Table
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 
 
-association_table = Table("place_amenity", Base.metadata,
-                          Column("place_id", String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True, nullable=False),
-                          Column("amenity_id", String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True, nullable=False))
-
+many_to_many = Table("place_amenity", Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True),
+                      Column('aminty_id'), String(60), ForeignKey('amenities.id', primary_key=True))
 
 class Place(BaseModel, Base):
     """Represents a Place for a MySQL database.
@@ -57,6 +47,7 @@ class Place(BaseModel, Base):
     reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity",
                              viewonly=False)
+    
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
